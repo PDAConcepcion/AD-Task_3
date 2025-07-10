8cf3068ac29cc79639d8fc50fbd90d11ca29dbec
PDAConcepcion <202311772@fit.edu.ph>
2025-07-11 00:07:57 +0800

    feat: alot of additonal for paths

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,15 @@
 <?php
 
-define('BASE_PATH',realpath(__DIR__));
+define('BASE_PATH', realpath(__DIR__));
+define('HANDLERS_PATH', realpath(BASE_PATH . '/handlers'));
+define('UTILS_PATH', realpath(BASE_PATH . '/utils'));
+define('DUMMIES_PATH', realpath(BASE_PATH . '/staticDatas'));
+define('COMPONENTS_PATH', realpath(BASE_PATH . '/components'));
+define('LAYOUTS_PATH', realpath(BASE_PATH . '/layouts'));
+define('PAGES_PATH', realpath(BASE_PATH . '/pages'));
+define('C_ASSETS_PATH', realpath(COMPONENTS_PATH . '/assets'));
+define('P_ASSETS_PATH', realpath(PAGES_PATH . '/assets'));
 
 chdir(BASE_PATH);
+
+session_start();
